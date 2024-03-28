a9569500b00d73385499a015b965bb4220747181
vSKAH <vskahhh@gmail.com>
2024-03-28 14:06:54 +0100

    fix(filename): Change the name of the file so that it is the same as in the topic

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
+	std::string str;
+	for (int i = 1; i < argc; i++) {
+		for (int j = 0; argv[i][j]; j++) {
+			str.append(1,  static_cast<char>(std::toupper(argv[i][j])));
+		}
+	}
+	std::cout << str << std::endl;
+}
